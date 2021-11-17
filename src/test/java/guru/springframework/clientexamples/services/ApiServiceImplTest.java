71c34a48cd74437d2c3c4394e6ae2943ab562a72
berkson <berksonx@yahoo.com.br>
2021-11-16 23:24:54 -0300

    conexão e testes com a api e flux teste. Closes #1

--- src/test/java/guru/springframework/clientexamples/services/ApiServiceImplTest.java
+++ src/test/java/guru/springframework/clientexamples/services/ApiServiceImplTest.java
@@ -22,7 +22,7 @@ import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 @ExtendWith(SpringExtension.class)
-class apiServiceImplTest {
+class ApiServiceImplTest {
 
     @Autowired
     Apiservice apiservice;
